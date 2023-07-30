f8585cda219def43a47d15dfd3cc2f598030b0b6
Jean-Luc <jeanlucv@berkeley.edu>
2023-07-29 19:28:08 -0700

    addrole function

--- index.js
+++ index.js
@@ -106,7 +106,42 @@ async function addDepartment() {
 init();
 }
 
+//add role function
+async function promptRoleDetails() {
+  const [departments] = await pool.query('SELECT * FROM department');
+  const departmentChoices = departments.map(department => ({ name: department.name }))
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "Enter the role name:",
+    },
+    {
+      type: "input",
+      name: "salary",
+      message: "Enter the role salary:",
+    },
+    {
+      type: "list",
+      name: "department",
+      message: "Select a department:",
+      choices: departmentChoices
+    },
+  ]);
+}
 
+async function addRole() {
+  const roleDetails = await promptRoleDetails();
+  const departmentIdQuery = 'SELECT id FROM department WHERE name = ?';
+  const [department] = await pool.query(departmentIdQuery, [roleDetails.department]);
+
+  const sql = 'INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)';
+  const values = [roleDetails.name, roleDetails.salary, department[0].id];
+  await pool.query(sql, values);
+
+  console.log('Role added to the database.');
+  init();
+}
 
 
 //end
